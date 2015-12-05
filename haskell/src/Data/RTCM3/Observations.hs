d6ff88297451f614ef335115d4e2f1ff3c4b0b8e
Mark Fine <mark.fine@gmail.com>
2015-12-04 22:24:24 -0800

    More progress.

--- haskell/src/Data/RTCM3/Observations.hs
+++ haskell/src/Data/RTCM3/Observations.hs
@@ -19,62 +19,62 @@ msg1001 :: Word16
 msg1001 = 1001
 
 data Msg1001 = Msg1001
-  {
+  { _msg1001_num :: Word16
   } deriving ( Show, Read, Eq )
 
 $(makeLenses ''Msg1001)
 
 instance Binary Msg1001 where
-  get = return Msg1001
+  get = return $ Msg1001 msg1001
 
   put _ = return ()
 
-$(deriveRTCM3 'msg1001 ''Msg1001)
+$(deriveRTCM3 ''Msg1001)
 
 msg1002 :: Word16
 msg1002 = 1002
 
 data Msg1002 = Msg1002
-  {
+  { _msg1002_num :: Word16
   } deriving ( Show, Read, Eq )
 
 $(makeLenses ''Msg1002)
 
 instance Binary Msg1002 where
-  get = return Msg1002
+  get = return $ Msg1002 msg1002
 
   put _ = return ()
 
-$(deriveRTCM3 'msg1002 ''Msg1002)
+$(deriveRTCM3 ''Msg1002)
 
 msg1003 :: Word16
 msg1003 = 1003
 
 data Msg1003 = Msg1003
-  {
+  { _msg1003_num :: Word16
   } deriving ( Show, Read, Eq )
 
 $(makeLenses ''Msg1003)
 
 instance Binary Msg1003 where
-  get = return Msg1003
+  get = return $ Msg1003 msg1003
 
   put _ = return ()
 
-$(deriveRTCM3 'msg1003 ''Msg1003)
+$(deriveRTCM3 ''Msg1003)
 
 msg1004 :: Word16
 msg1004 = 1004
 
 data Msg1004 = Msg1004
-  {
+  { _msg1004_num :: Word16
   } deriving ( Show, Read, Eq )
 
 $(makeLenses ''Msg1004)
 
 instance Binary Msg1004 where
-  get = return Msg1004
+  get = return $ Msg1004 msg1004
 
   put _ = return ()
 
-$(deriveRTCM3 'msg1004 ''Msg1004)
+$(deriveRTCM3 ''Msg1004)
--- haskell/src/Data/RTCM3/TH.hs
+++ haskell/src/Data/RTCM3/TH.hs
@@ -10,23 +10,19 @@
 
 module Data.RTCM3.TH where
 
-import           BasicPrelude hiding ( length )
-import           Control.Lens
-import           Data.Binary
-import           Data.Binary.Put
-import qualified Data.Binary.Bits.Put as B
-import           Data.ByteString.Lazy
-import           Data.RTCM3.Types
-import           Language.Haskell.TH
+import BasicPrelude hiding ( length )
+import Control.Lens
+import Data.Binary
+import Data.ByteString.Lazy
+import Data.RTCM3.Types
+import Language.Haskell.TH
 
 -- | Derive ToRTCM3 typeclass, given an RTCM3 message number name and the
 -- name of the implemented type.
-deriveRTCM3 :: Name -> Name -> Q [Dec]
-deriveRTCM3 num name =
+deriveRTCM3 :: Name -> Q [Dec]
+deriveRTCM3 name =
   [d|instance ToRTCM3 $(conT name) where
        toRTCM3 msg = encoded & msgRTCM3Crc .~ checkCrc encoded where
-         payload = runPut $ B.runBitPut $ do
-           B.putWord16be 10 $(varE num)
-           B.putByteString $ toStrict $ encode msg
+         payload = encode msg
          encoded = Msg (fromIntegral $ length payload) (toStrict payload) 0
     |]
